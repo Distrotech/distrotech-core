a0b8eacec042661acd54dbbf95ca90cc6edb8ecd
admin <admin@a03fbada-158d-47ef-9f12-77e997805b15>
2012-06-07 11:52:58 +0000

    cast lpad SQL Function usr/sbin/genconf dont set slapd threads subnet decleration for 0.0.0.0/32 and dummy 0 (dhcpd)
    git-svn-id: http://ns2.distrotech.co.za/svnsrc/distrotech-core/trunk@1465 a03fbada-158d-47ef-9f12-77e997805b15

---
+++ var/spool/apache/htdocs/hotspot/user_tmpl.php
@@ -0,0 +1,5 @@
+<%
+include "session.inc";
+%>
+<script language="JavaScript" src="fct.js" type="text/javascript"></script>
+<A HREF="javascript:popUpWindow('<%print $_SESSION['uam_url']%>/prelogin','GLS','270','325',0,0,0,0);">Access</A>
