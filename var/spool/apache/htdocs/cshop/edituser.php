96668e1fbca097edb397d9e50869e016c0a78b69
admin <admin@a03fbada-158d-47ef-9f12-77e997805b15>
2012-06-30 12:35:51 +0000

    Seperate PIN and linepass on edit userpage
    git-svn-id: http://ns2.distrotech.co.za/svnsrc/distrotech-core/trunk@1513 a03fbada-158d-47ef-9f12-77e997805b15

--- var/spool/apache/htdocs/cshop/edituser.php
+++ var/spool/apache/htdocs/cshop/edituser.php
@@ -29,7 +29,7 @@ if (isset($_POST['saveuser'])) {
   } else {
     $_POST['ivrwarn']=sprintf("%d",$_POST['ivrwarn']*10000);
   }
-  pg_query("UPDATE users SET password='" . $_POST['userpass'] . "',secret='" . $_POST['userpass'] . "',fullname='" . $_POST['firstname'] . "',ivrwarn=" . $_POST['ivrwarn'] .
+  pg_query("UPDATE users SET password='" . $_POST['userpass'] . "',secret='" . $_POST['linepass'] . "',fullname='" . $_POST['firstname'] . "',ivrwarn=" . $_POST['ivrwarn'] .
         ",tariff='" . $_POST['tariff'] . "',callerid= '" . $_POST['defcli'] . "',email= '" . $_POST['email'] . "',activated = '" . $_POST['acact'] . 
         "',simuse='" .  $_POST['simuse'] . "' WHERE name='" . $_SESSION['number'] . "'");
 } else if ($_POST['deluser'] == "1") {
@@ -47,7 +47,7 @@ if (isset($_POST['saveuser'])) {
 }
 
 
-$boothq=pg_query($db,"SELECT password,fullname,tariff,email,activated,simuse,ivrwarn,callerid
+$boothq=pg_query($db,"SELECT password,fullname,tariff,email,activated,simuse,ivrwarn,callerid,secret
                      FROM users
                      WHERE usertype=1 AND users.name='" . $_SESSION['number'] . "'
                            AND agentid = " . $_SESSION['resellerid'] . " LIMIT 1");
@@ -62,19 +62,21 @@ $user=pg_fetch_row($boothq,0);
 <TR CLASS=list-color1>
 <TD>Pin</TD><TD><INPUT TYPE=TEXT NAME=userpass VALUE="<%print $user[0];%>"></TD></TR>
 <TR CLASS=list-color2>
+<TD>Line Password</TD><TD><INPUT TYPE=TEXT NAME=linepass VALUE="<%print $user[8];%>"></TD></TR>
+<TR CLASS=list-color1>
 <TD>User Name</TD><TD><INPUT TYPE=TEXT NAME=firstname VALUE="<%print $user[1];%>"></TD></TR>
-<TR CLASS=list-color1>
+<TR CLASS=list-color2>
 <TD>Email Address</TD><TD><INPUT TYPE=TEXT NAME=email VALUE="<%print $user[3];%>"></TD></TR>
-<TR CLASS=list-color2>
-<TD>Default Caller ID</TD><TD><INPUT TYPE=TEXT NAME=defcli VALUE="<%print $user[7];%>"></TD></TR>
 <TR CLASS=list-color1>
-<TD>Simuse</TD><TD><INPUT TYPE=TEXT NAME=simuse VALUE="<%print $user[5];%>"></TD></TR>
+<TD>Default Caller ID</TD><TD><INPUT TYPE=TEXT NAME=defcli VALUE="<%print $user[7];%>"></TD></TR>
 <TR CLASS=list-color2>
+<TD>Simuse</TD><TD><INPUT TYPE=TEXT NAME=simuse VALUE="<%print $user[5];%>"></TD></TR>
+<TR CLASS=list-color1>
 <TD>Warn User When Call Goes Bellow</TD><TD>
 <INPUT TYPE=TEXT NAME=ivrwarn VALUE="<%if ($user[6] > 0) {printf("%0.2f",$user[6]/10000);} else if ($user[6] < 0) {print "";} else { print "0";}%>"></TD></TR>
-<TR CLASS=list-color1>
-<TD>Activated</TD><TD><INPUT TYPE=CHECKBOX NAME=acact<%if ($user[4] == "t") {print " CHECKED";};%>></TD></TR>
 <TR CLASS=list-color2>
+<TD>Activated</TD><TD><INPUT TYPE=CHECKBOX NAME=acact<%if ($user[4] == "t") {print " CHECKED";};%>></TD></TR>
+<TR CLASS=list-color1>
 <TD>Rate Plan</TD><TD><SELECT NAME=tariff><%
   $tplan=pg_query($db,"SELECT tariffname,tariffcode FROM tariff WHERE tariffcode LIKE '" .
                        $_SESSION['resellerid'] . "-%' ORDER BY tariffname");
@@ -91,7 +93,7 @@ $user=pg_fetch_row($boothq,0);
 %>
 </SELECT>
 </TD></TR>
-<TR CLASS=list-color1>
+<TR CLASS=list-color2>
 <TD ALIGN=MIDDLE COLSPAN=2>
 <INPUT TYPE=SUBMIT onclick=this.name='saveuser' VALUE="Save Changes">
 <INPUT TYPE=SUBMIT onclick=this.name='vladmin' VALUE="Advanced">
