14241fe1b840f8ba27af3db85b7a3a42083d712f
admin <admin@a03fbada-158d-47ef-9f12-77e997805b15>
2012-07-21 18:52:06 +0000

    Add support for deflection in cc_callerid Drop unused views and extend size of name/defaultuser to accomodate long hostnames increase size of ipaddr to cater for ipv6
    git-svn-id: http://ns2.distrotech.co.za/svnsrc/distrotech-core/trunk@1609 a03fbada-158d-47ef-9f12-77e997805b15

---
+++ var/spool/update/2012-07-18.cc_callerid.sql
@@ -0,0 +1,1 @@
+ALTER TABLE cc_callerid ADD deflect varchar(128);
---
+++ var/spool/update/2012-07-21.users.sql
@@ -0,0 +1,4 @@
+ALTER TABLE users ALTER defaultuser TYPE varchar(32);
+DROP VIEW sip ;
+DROP VIEW testview;
+ALTER TABLE users ALTER name TYPE varchar(32);
