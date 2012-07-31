e976af8e50367d6fa14169d1c459a413d7cfd301
admin <admin@a03fbada-158d-47ef-9f12-77e997805b15>
2012-07-31 07:23:57 +0000

    Increase size of transport,name,h323gkid,h323reggk for long hostnames
    git-svn-id: http://ns2.distrotech.co.za/svnsrc/distrotech-core/trunk@1628 a03fbada-158d-47ef-9f12-77e997805b15

---
+++ var/spool/update/2012-07-30.users.sql
@@ -0,0 +1,1 @@
+ALTER TABLE users ALTER transport TYPE varchar(16);
---
+++ var/spool/update/2012-07-31.users.sql
@@ -0,0 +1,5 @@
+ALTER TABLE users ALTER defaultuser TYPE varchar(64);
+ALTER TABLE users ALTER name TYPE varchar(64);
+ALTER TABLE users ALTER h323gkid TYPE varchar(64);
+ALTER TABLE trunk ALTER h323gkid TYPE varchar(64);
+ALTER TABLE trunk ALTER h323reggk TYPE varchar(64);
